9d33025a1034e0e2d02d82c6787786e043c86a2b
Evan M Jones <evan@crowdhubapps.com>
2019-01-28 18:02:47 -0800

    Fix(Keybind spacebar): Don't preventDefault on space keybind.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { eventStop } from "./constants";
 keybinds([], 39, () => bus.emit("right"));
 keybinds([], 37, () => bus.emit("left"));
 keybinds([], 27, eventStop(() => bus.emit("toggleDisplay")));
-keybinds([], 32, eventStop(() => bus.emit("togglePlayback")));
+keybinds([], 32, () => bus.emit("togglePlayback"));
 keybinds([17], 83, eventStop(() => bus.emit("download")));
 
 mouse()
