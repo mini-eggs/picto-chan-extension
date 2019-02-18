884f78d432933b8ded4fff8595b3aaf65b4e3d9b
Evan M jones <evan@localhost.localdomain>
2019-02-17 22:46:01 -0700

    Feat(Controls): Adding nano/emacs prev/next.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import mouse from "mouse-activity";
 import { eventStop } from "./constants";
 
 keybinds([], 39, () => bus.emit("right"));
+keybinds([17], 70, eventStop(() => bus.emit("right")));
 keybinds([], 37, () => bus.emit("left"));
+keybinds([17], 66, () => bus.emit("left"));
 keybinds([], 27, eventStop(() => bus.emit("toggleDisplay")));
+keybinds([17], 71, eventStop(() => bus.emit("toggleDisplay")));
 keybinds([], 32, () => bus.emit("togglePlayback"));
 keybinds([17], 83, eventStop(() => bus.emit("download")));
+
+
 
 mouse()
   .active(() => bus.emit("mouseActive"))
